aaaf4fba5704aff7ca0723f0adffaf2a14bf5a33
Jia Cheng <dazzed17@gmail.com>
2022-09-11 21:38:06 +0800

    added home screen and product screen

---
+++ frontend/src/data.js
@@ -0,0 +1,32 @@
+const data = {
+  products: [
+    {
+      name: "Chocolate Chip Cookies",
+      slug: "chocolate-chip-cookies",
+      category: "Chips",
+      image: "/images/p1.jpg",
+      price: 1,
+      countInStock: 10,
+      description: "Freshly baked chocolate chips",
+    },
+    {
+      name: "Strawberry Chip Cookies",
+      slug: "strawberry-chip-cookies",
+      category: "Chips",
+      image: "/images/p2.jpg",
+      price: 1,
+      countInStock: 10,
+      description: "Freshly baked strawberry chips",
+    },
+    {
+      name: "Blueberry Chip Cookies",
+      slug: "blueberry-chip-cookies",
+      category: "Chips",
+      image: "/images/p3.jpg",
+      price: 1,
+      countInStock: 10,
+      description: "Freshly baked blueberry chips",
+    },
+  ],
+};
+export default data;
