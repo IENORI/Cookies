e5b9d78f0952818e7403afa31cea0040e5f484e8
chenghao99 <leechenghao1997@gmail.com>
2022-10-26 17:54:56 +0800

    add 1 sign in test case

--- frontend/test/test.js
+++ frontend/test/test.js
@@ -1,25 +1,43 @@
 var should = require('should');
 var request = require('request');
 var expect = require('chai').expect;
+var chai = require('chai');
 var util = require('util');
 
-// testing sign in function
-describe('captcha error test', function () {
-  it('captcha error test', function (done) {
-    request.post(
-      {
-        url: 'http://localhost:5000/api/users/signin',
-        form: {
-          token: 'invalid_token',
-          email: 'JC@email.com',
-          password: 'test_password',
+describe('sign in unit test', function () {
+  // testing sign in function
+  describe('recaptcha fail verify test', function () {
+    it('recaptcha return 401 error code', function (done) {
+      request.post(
+        {
+          url: 'http://localhost:5000/api/users/signin',
+          form: {
+            token: 'invalid_token',
+            email: 'JC@email.com',
+            password: 'test_password',
+          },
         },
-      },
-      function (error, response, body) {
-        expect(response.statusCode).to.equal(401);
-        console.log(body);
-        done();
-      }
-    );
+        function (error, response, body) {
+          expect(response.statusCode).to.equal(401);
+          done();
+        }
+      );
+    });
+  });
+
+  // testing recaptcha pass. api key is testing key not actual key
+  describe('captcha pass test', function () {
+    it('recaptcha return should be success', function (done) {
+      request.post(
+        {
+          url: 'https://www.google.com/recaptcha/api/siteverify?secret=6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe&response=anytoken',
+        },
+        function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          expect(JSON.parse(response.body).success).to.equal(true);
+          done();
+        }
+      );
+    });
   });
 });
