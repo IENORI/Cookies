be8a602ca9c534f3c8f394c27f970f57dcf62ffa
chenghao99 <leechenghao1997@gmail.com>
2022-11-01 15:40:06 +0800

    update

--- backend/test/registerTest.js
+++ backend/test/registerTest.js
@@ -16,8 +16,7 @@ describe('register account unit test', function () {
           },
         },
         function (error, response, body) {
-          console.log("hi");
-          console.log("body is: ", response)
+          console.log("HEHEHEHEHE")
           expect(JSON.parse(response.body).message).to.equal(
             'Invalid email format!'
           );
@@ -27,4 +26,37 @@ describe('register account unit test', function () {
     });
   });
 
+  // testing register function with valid email
+  describe('valid input fields test', function () {
+    it("signup should return 'created account!'", function (done) {
+      request.post(
+        {
+          url: 'http://localhost:5000/api/users/signup',
+          form: {
+            name: 'testname',
+            email: 'testcaseUser@gmail.com',
+            password: 'Testpassword1',
+            confirmPassword: 'Testpassword1',
+          },
+        },
+        function (error, response, body) {
+          expect(JSON.parse(response.body).hasOwnProperty('_id')).to.equal(
+            true
+          );
+          const id = JSON.parse(response.body)._id;
+          const token = JSON.parse(response.body).token;
+          request.delete(
+            {
+              url: `http://localhost:5000/api/users/deleteuser/${id}`,
+              headers: { authorization: `Bearer ${token}` },
+            },
+            function (error, response, body) {
+              expect(response.body).to.equal('user deleted');
+            }
+          );
+        }
+      );
+      done();
+    });
+  });
 });
