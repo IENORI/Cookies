bdd566c2c59b442dda9e8157b1ff55912e034093
Jun Hui <fjh828@gmail.com>
2022-11-06 19:46:08 +0800

    Cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
 import express from "express";
-// import data from "./data.js";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
@@ -38,7 +36,6 @@ app.get(
   })
 );
 
-// app.use("/api/seed", seedRouter); //if route matches /api/seed -> callback
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
